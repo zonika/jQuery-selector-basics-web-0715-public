e001a11ae948efedcd41904bdd8917218fbb4cb9
zonika <zchodosh@conncoll.edu>
2015-09-14 14:51:02 -0400

    done

--- js/selectors.js
+++ js/selectors.js
@@ -8,3 +8,18 @@ $(document).ready(function(){
 });
 
 // declare your functions here...
+function h1Selector(){
+  return $('h1');
+}
+function liInOlSelector(){
+  return $('ol > li')
+}
+function linkSelector() {
+  return $('#box4.box5 > a')
+}
+function imageSelector() {
+  return $('img[alt ="cat sleeping"]')
+}
+function checkboxInputSelector() {
+  return $(':checkbox').wrap('<span class="pinky"></span>')
+}
